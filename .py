3a85cf9f903716df8260d628adcbad5ec413bc99
ParsaGg0 <159770243+ParsaGg0@users.noreply.github.com>
2024-11-06 20:57:47 +0330

    hi , guys I hope you enjoy my pthon project
    let me know , if I had a mistakes! 😊

---
+++ .py
@@ -0,0 +1,11 @@
+# projeye 🚀پروژه: پرتاب کردن سکه🚀
+import random 
+int=('seke')
+for i in range(0,1001):
+   x=random.randint(1,2):
+   if x==1:
+        roo +=1
+    else:
+       posht +=1
+print("ROO",roo)
+print("POSHT,posht")
